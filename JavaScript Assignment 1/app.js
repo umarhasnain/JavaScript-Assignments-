6aefb06cb612b11212379cb991b7125c0be70a92
Umar Hasnain <103918550+umarhasnain@users.noreply.github.com>
2024-03-03 01:13:01 +0500

    JS-Assignment Chap 1,2 & 3

---
+++ JavaScript Assignment 1/app.js
@@ -0,0 +1,109 @@
+//***************************** Chapter # 01 *************************
+
+
+//                   Question # 01
+
+alert("WellCome");
+
+//                   Question # 02
+
+alert("Error! Please enter a valid password.");
+
+//                   Question # 03
+
+alert("Welcome to JS Land...\n Happy coding!");
+
+//                   Question # 04
+
+alert("Welcome to JS Land");
+
+//                   Question # 05
+
+alert("Happy Coding!...\n Prevent this page from creating additional dialogos.");
+
+//                   Question # 06
+
+alert("Hellow.. I can run JS through my web browser,s console");
+
+
+
+
+//***************************** Chapter # 02 *************************
+
+
+//                   Question # 01
+
+var username
+
+//                   Question # 02
+
+var myname = "Umar Hasnain"
+
+//                   Question # 03
+
+var massage = "Hello World";
+ alert(massage) 
+
+//                   Question # 04
+
+var std = "Jhone Doe"
+var age = "15 years old";
+var course = "Certified Web & Mobile App Development";
+alert(std);
+alert(age);
+alert(course)
+
+//                   Question # 05
+
+ var msg = "pizza";
+ alert("PIZZA...\n PIZZ...\n PIZ...\n PI...\n P");
+
+
+//                   Question # 06
+
+ var email = "umarhasnain045@gmail.com";
+ alert("My Email address is:" + " " + email)
+
+ 
+//                   Question # 07
+
+var Book = "A Smarter way to Learn JavaScript";
+alert("I am trying to learn from Book " + Book)
+
+
+//                  Question # 08
+var msg =  " Task 8: "+ "Yah! I can write HTML content through JavaScript";
+document.write(msg)
+
+//                  Question # 09
+var msg = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(msg)
+
+
+
+//***************************** Chapter # 03 *************************
+
+//                  Question # 01
+
+ var age = "I am 15 years old";
+ alert(age)
+
+
+//                  Question # 02
+
+ var user = "You have visited this site 14 times";
+ alert(user)
+
+//                  Question # 03
+
+ var birthyear = "My BirthYear is 1990  " 
+ document.write(birthyear) 
+
+
+//                  Question # 04
+
+ var name = prompt("Enter Your Name");
+ var quantity = prompt("how many products a you wants to order");
+ var product_title = prompt("Enter Your Product Title");
+
+ document.write(name + "Ordered " + quantity + "On " + product_title)
