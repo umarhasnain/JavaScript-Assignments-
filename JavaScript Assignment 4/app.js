75451a422474fe67ac4eb3cbe44fa38e32c47355
Umar Hasnain <103918550+umarhasnain@users.noreply.github.com>
2024-03-17 17:31:48 +0500

    JS-Assignment Chap 14 - 16

---
+++ JavaScript Assignment 4/app.js
@@ -0,0 +1,146 @@
+// *******************   Chapter 14-16 *****************
+
+// ******************* Question # 1 *****************
+// var arr = new array();
+
+// ******************* Question # 2 *****************
+// var arr = [];
+
+// ******************* Question # 3 *****************
+// var str = ["Khi","Lahore","Isl"];
+
+// ******************* Question # 4 *****************
+// var num = [1,2,3,4,5];
+
+// ******************* Question # 5 *****************
+// var boll = [true,false];
+
+// ******************* Question # 6 *****************
+// var arr = ["Khi", 12,true];
+
+
+// ******************* Question # 7 *****************
+// var arr = ["SSC","HSC","BSC","BS","BCOM","MSC","M.PHILL","PHD"];
+
+// document.write("<h1>"+ arr[0] + "</h1>")
+// document.write("<h1>"+ arr[1] + "</h1>")
+// document.write("<h1>"+ arr[2] + "</h1>")
+// document.write("<h1>"+ arr[3] + "</h1>")
+// document.write("<h1>"+ arr[4] + "</h1>")
+// document.write("<h1>"+ arr[5] + "</h1>")
+// document.write("<h1>"+ arr[6] + "</h1>")
+// document.write("<h1>"+ arr[7] + "</h1>")
+
+
+// ******************* Question # 8 *****************
+
+// var stdName = ["Umar","Hasnain","Ali"];
+// var score = ["200","300","400"] ;
+
+// document.write("Score of "+ stdName[0]+ " is " + score[0] +"</br>" ) 
+// document.write("Score of "+ stdName[1]+ " is " + score[1]+"</br>")
+// document.write("Score of "+ stdName[2]+ " is " + score[2]+"</br>")
+
+// ******************* Question # 9 *****************
+
+// var arr = ["Red" , "Green" ,"Yellow"];
+// var user = prompt("In which colour you wants to add in the array");
+// var newUser = user
+// //(a)
+// // arr.unshift("White","Blue")
+// arr.unshift(newUser)
+
+// document.write(arr)
+
+// (b)
+
+// var arr = ["Red" , "Green" ,"Yellow"];
+// var user = prompt("In which colour you wants to add in the array");
+// var newUser = user
+
+// arr.push(newUser)
+// document.write(arr)
+
+//(C)
+// var arr = ["Red" , "Green" ,"Yellow"];
+// arr.unshift("Gray","Pink");
+
+// document.write(arr)
+
+//(d)
+// var arr = ["Red" , "Green" ,"Yellow"];
+// arr.shift(arr);
+
+// document.write(arr)
+
+//(e)
+// var arr = ["Red" , "Green" ,"Yellow"];
+// arr.pop(arr)
+
+// document.write(arr)
+
+//(f)
+// var arr = ["Red" , "Green" ,"Yellow"];
+// var user = prompt("In which colour you wants to add in the array");
+// var newUser = user
+
+// arr.splice(2,0, newUser)
+// document.write(arr)
+
+//(g)
+
+// var arr = ["Red" , "Green" ,"Yellow","Pink","Brown","Blue"];
+// var user = +prompt("In which index no you wants to remove in the array");
+// var newUser = user
+
+// arr.splice(2,newUser)
+// document.write(arr)
+
+
+// ******************* Question # 10 *****************
+
+
+// var arr = [320,230,480,120];
+// document.write("Scores of Student " + arr + "<br>")
+
+// var arr = [320,230,480,120].sort()
+// document.write("Ordered Scores of Student " + arr)
+
+// ******************* Question # 11 *****************
+
+// var arr = ["karachi","hederabad","lahore","multan","islamabad"];
+// var user = prompt("Select index no to select city");
+// var userSecond = prompt("Select  Second index no to select city");
+
+// document.write(arr[user] + " " + arr[userSecond])
+
+// ******************* Question #  12*****************
+
+// var arr = ["This", "is", "my","cat"];
+// // arr.join(",")
+// document.write(arr.join(" "))
+
+// ******************* Question # 13 *****************
+
+// var arr = [];
+// arr.push("keyboard","mouse","printer","monitar");
+// document.write(arr[0]+ "<br>")
+// document.write(arr[1]+ "<br>")
+// document.write(arr[2]+ "<br>")
+// document.write(arr[3]+ "<br>")
+
+// ******************* Question # 14 *****************
+
+// var arr = [];
+// arr.push("keyboard","mouse","printer","monitar");
+// document.write(arr[3]+ "<br>")
+// document.write(arr[2]+ "<br>")
+// document.write(arr[1]+ "<br>")
+// document.write(arr[0]+ "<br>")
+
+
+// ******************* Question # 15 *****************
+
+// var arr = ["Apple","Samsung", "Motorola", "Nokia", "Sony" , "Haier"];
+// console.log(arr)
+
